c3fe5fec81df95d81796bee76f1b8dfbee8a9e15
dandamasek <76252963+dandamasek@users.noreply.github.com>
2024-05-23 19:29:11 +0200

    update without personal path

--- src/dspace_documents/config.py
+++ src/dspace_documents/config.py
@@ -1,6 +1,6 @@
 # config of DSpace app
 DSPACE_DOMAIN = "http://127.0.0.1"
 DSPACE_PORT = "8000"
-BASE_FOLDER = r"C:\\Users\\masek\\Dokumenty\\codes\\documents_ws\\test_pdf"
-COLLECTION_ID = "fd6abc5b-a4e4-4dae-a3fc-b3d5b3aa9325"
+BASE_FOLDER = r"C:\\dir\\file.pdf"
+COLLECTION_ID = ""
 FILE_FORMAT = ".pdf"
